88b8458ae961f830e5c47a7f5cce56fd33fb6c3b
imranyy <imranmatano6@gmail.com>
2022-10-10 16:26:49 +0300

    first step now add targets and msg

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const puppeteer=require('puppeteer');
 
+async function scrape(url){
+    const browser=await puppeteer.launch({headless:false});
+    const page=await browser.newPage();
+    await page.goto(url);
+    await page.waitForSelector("");
+    const target =await page.$("");
+    await target.click();
+    const inputBox=await page.$("");
+    //the loops
+    for(let i=0; 1<70;i++) {
+        await inputBox.type("hello loser");
+        await page.keyboard.press("Enter");
+    }
+
+}
+scrape("https://web.whatsapp.com");
